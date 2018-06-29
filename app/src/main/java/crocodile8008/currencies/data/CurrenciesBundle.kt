f49d74e95d9e1573be20c4a669dc0dbdf4069b1a
Andrey <crocodile8000@gmail.com>
2018-06-30 01:05:53 +0300

    Work.

--- app/src/main/java/crocodile8008/currencies/data/CurrenciesBundle.kt
+++ app/src/main/java/crocodile8008/currencies/data/CurrenciesBundle.kt
@@ -7,4 +7,10 @@ data class CurrenciesBundle(
         val base : String,
         val date : String = "1970-01-01",
         val rates : Map<String, Float> = HashMap()
-)
+) {
+    companion object {
+        val EMPTY = CurrenciesBundle("")
+    }
+
+    fun isEmpty() = this == EMPTY
+}
